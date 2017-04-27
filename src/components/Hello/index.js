3097b6ac020898669439d9ed77337d420ae6921b
Ashok Vijayendra <ashok.vijay.blr@gmail.com>
2017-04-27 12:49:51 -0500

    removing unneeded files

--- src/components/Hello/index.js
+++ src/components/Hello/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import styles from './Hello.css';
+
 const Hello = ({name}) => (
-    <h1 className="Hello__heading">Heloooo, {name}</h1>
+    <h1 className="Hello__heading">Hellooo, {name}</h1>
 );
 
 export default Hello;
